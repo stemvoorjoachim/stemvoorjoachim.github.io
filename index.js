0dd791229562229bec703eb226d66d5a2d17672b
Joël <25136@greijdanus.nl>
2025-01-17 11:49:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-//hier komt mijn code
+//Ik ben iets aan het doen Nielson!!!
+let informatie = document.getElementById("info");
+let standpunten = document.getElementById("standpunt");
+let verandering = document.getElementById("verandering");
+let tekst = document.getElementById("stem")
+
+informatie.onClick = function() {
+  tekst.textcontent = `Supergaaf`;
+}
